51bc6c213454639adcebae44a6b93f604d8c88e3
Toodyao-XPS-13 <toodyao@gmail.com>
2018-07-01 22:33:57 +0800

    Finish RAM

--- ram/ad_control.v
+++ ram/ad_control.v
@@ -2,11 +2,19 @@ module ad_control(BHE, A0, in_ad, out_odd, out_even);
 
 input BHE, A0;
 input[15:0] in_ad;
-output[7:0] out_odd, out_even;
+output reg[15:0] out_odd, out_even;
 
-always @(BHE or A0)
+always @(BHE)
 begin
+	if(~BHE)
+		begin out_even<= in_ad>>1; end
+end
 
+always @(A0)
+begin
+	// out_odd<={1'b0, in_ad[15:9]};
+	if (~A0)
+		begin out_odd<=in_ad>>1; end
 end
 
 endmodule
--- ram/data_invert.v
+++ ram/data_invert.v
@@ -1,21 +1,24 @@
-module data_invert(ram_invert, BHE, A0, odd, even, out);
+module data_invert(ram_invert, odd, even, out);
 
-input ram_invert, BHE, A0;
+input ram_invert;
+// input BHE, A0;
 input[7:0] odd, even;
 output [15:0] out;
 
-reg [7:0]odd_r, even_r;
+// reg [7:0]odd_r, even_r;
 
-always @(posedge BHE)
-begin
-	even_r<=even;
-end
+// always @(even)
+// begin
+// 	even_r<=even;
+// end
 
-always @(posedge A0)
-begin
-	odd_r<=odd;
-end
+// always @(odd)
+// begin
+// 	odd_r<=odd;
+// end
 
-assign out = ram_invert ? {even_r, odd_r} : {odd_r, even_r};
+// assign out = ram_invert ? {even_r, odd_r} : {odd_r, even_r};
+
+assign out = ram_invert ? {even, odd} : {odd, even};
 
 endmodule
